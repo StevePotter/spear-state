78c67d14ee9a4c96225feac803a427eb0dfce41d
Stephen Potter <steve.potter@vydia.com>
2018-03-18 09:47:07 -0400

    more tests.  change notification

--- src/index.js
+++ src/index.js
@@ -27,65 +27,91 @@ appState.ui.example = 'one two'
 
 That would result in first appSate, then appState.ui
 
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
 
 Structure:
 - if a scalar is assigned to a leaf node, it's value is kept inside the parent proxy
 - if a node is unassigned, it is treated as an object.  like createStore().one.two - two is a proxy object and can be assigned to
-- console.log is not working because the object returned
-- ideall
 
 to do this, you have nodes in a tree.  intermediate nodes use a Proxy
 
-{
-   first: {
-    __info: {}
-
-  }
-
-}
-
-
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
+random notes:
+in Proxy get trap, an array index is passed in as a string, like '0'
+would be nice to bulk change for react performance
 */
 
-
 export function createStore(initialState) {
-  return stateNode(0, initialState || {}, null, 'root')
+  return stateNode(0, initialState ? Object.assign({}, initialState) : {}, null, null, 'root')
 }
 
 
 
-function stateNode(level, baseState, parentProxy, name) {
-  const childProxies = {}
-  const builtInFunctions = {
-    toString: () => '[StateNode]',
-    getState: () => baseState
-  }
+function stateNode(level, propertyValues, parentProxy, parentInternals, name) {
   const fullName = () => {
-    return `${root}@d${level}`
+    return `${name}@d${level}`
+  }
+
+  const internals = {
+    onChange: (propertyName) => {
+      console.log(`${fullName()} onChange for '${propertyName}'`, propertyValues)
+      if (childProxies.hasOwnProperty(propertyName)) {
+        // the change occured from within a child proxy
+        // to enable === checking in changes, we need to do a few chores
+        // change propertyValues reference
+        // todo: is this redundant at all and should have an edge case for the root?
+        propertyValues = Object.assign({}, propertyValues)
+        // change inner child reference.
+        propertyValues[propertyName] = Object.assign({}, propertyValues[propertyName])
+        console.log(`${fullName()} reassigning state and ${propertyName}`)
+      } else if (!parentProxy) {
+        console.log('need to change asdfasdf' +propertyName)
+
+      }
+      if (internals.subscribeCallback) {
+        console.log(`${fullName()} notifying of change on '${propertyName}'`, propertyValues)
+        internals.subscribeCallback()
+      }
+      if (parentInternals) parentInternals.onChange(name)
+    }
+  }
+
+  const childProxies = {}
+  let subscribeCallback
+
+  const externalFunctions = {
+    toString: () => '[StateNode]',
+    // you could just return `propertyValues`
+    getState: () => Object.freeze(Object.assign({},propertyValues)),
+    subscribe: (callback) => {
+      internals.subscribeCallback = callback // todo: multiple callbacks
+    },
+    // // changes the ent
+    // replace: () => {
+    //
+    // }
   }
   // you can't extend Proxy unfortunately
-  const proxy = new Proxy(baseState,
+  const proxy = new Proxy({},
     {
       // prop is the name of the property or indexer ('0')
       get: function(target, prop, receiver) {
-        if (builtInFunctions.hasOwnProperty(prop)) {
-          return builtInFunctions[prop]
+        if (externalFunctions.hasOwnProperty(prop)) {
+          return externalFunctions[prop]
         }
         // complex child should come before its value so you can drill down further in the object chain
         if (childProxies.hasOwnProperty(prop)) {
           return childProxies[prop]
         }
-        if (baseState.hasOwnProperty(prop)) {
+        if (propertyValues.hasOwnProperty(prop)) {
           console.log(`${fullName()}['${prop}'] with existing value`)
-          return baseState[prop]
+          return propertyValues[prop]
         }
         //
         console.log(`${fullName()}['${prop}'] is new state node`)
         //` proxy: ' + prop, target);
         const childState = {}
-        baseState[prop] = childState
-        const childProxy = stateNode(level + 1, childState, proxy, prop)
+        propertyValues[prop] = childState
+        const childProxy = stateNode(level + 1, childState, proxy, internals, prop)
         childProxies[prop] = childProxy
         return childProxy
       },
@@ -94,31 +120,17 @@ function stateNode(level, baseState, parentProxy, name) {
         // setting a value will set it on the
         //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy/handler/set
         console.log(`setting ${prop} at level ${level} to ${value}`)
-        baseState[prop] = value // should you use Reflect.set?
+        propertyValues[prop] = value // should you use Reflect.set?
         // if you do const a = app.first.second; const b = a.third; a.third = 12; // then third would have been a proxy first then reassigned as a scalar
         if (childProxies.hasOwnProperty(prop)) {
           console.log('already had child proxy.  deleting it')
           delete childProxies[prop]
         }
+        internals.onChange(prop)
+
         return true
       }
     }
   )
-  // proxy.toString = Function.prototype.toString.bind('hi')
-  // proxy.zzz = 3
   return proxy
 }
-
-//
-// function getChild(parent, prop) {
-//   return new Proxy(parent,
-//     {
-//       // prop is the name of the property or indexer ('0')
-//       get: function(target, prop, receiver) {
-//
-//         console.log('child proxy: ' + prop, target);
-//         return getChild(target, prop)
-//       }
-//     }
-//   )
-// }
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,7 @@
-import { expect } from 'chai'
+import chai, { expect } from 'chai'
+import spies from 'chai-spies'
 import { createStore } from '../src'
+chai.use(spies)
 
 /*
 tests to cover:
@@ -7,10 +9,12 @@ tests to cover:
 - setting a property to a complex object
 - handling array.  do we just treat those
 - subscribe to changes
+- handle delete operator (or don't)
+
 */
 
 describe('getState', () => {
-  it('should work', () => {
+  it('basic property setting', () => {
     const app = createStore()
     expect(app.getState()).to.eql({})
 
@@ -19,7 +23,27 @@ describe('getState', () => {
     const stateBefore = app.getState()
 
     expect(app.getState()).to.eql({ a1: { b1: 4 }})
+  })
 
+  it('setting property makes proper changes', () => {
+    const app = createStore()
+
+    app.a1.b1.c1 = 4
+    const stateBefore = app.getState()
+    // closest object makes change notification
+    const subscribeToRootChange = chai.spy(() => {})
+    app.subscribe(subscribeToRootChange)
+    const subscribeToLeafChange = chai.spy(() => {})
+    app.a1.subscribe(subscribeToLeafChange)
+    app.a1.b1.c1 = 5
+    const stateAfter = app.getState()
+
+    expect(subscribeToLeafChange).to.have.been.called()
+    expect(subscribeToRootChange).to.have.been.called()
+
+    expect(stateBefore.a1).not.to.equal(stateAfter.a1)
+    //expect(app.getState()).to.eql({ a1: { b1: 4 }})
+  })
 
 
     //
@@ -39,7 +63,7 @@ node level 1 get 'second'
 setting third at level 2 to 55
 */
     //expect(55).to.equal(55);
-  });
+//  });
 //   it('should work', () => {
 //     const app = createStore({ bbb: true })
 //     //const result = s.first.second
